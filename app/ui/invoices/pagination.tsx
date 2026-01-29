43fef0f72533943adf1382cbe0dbb9f4ced4c92a
Samuel581 <samuel.adonay2001@gmail.com>
2026-01-29 13:26:25 -0600

    :sparkles: Feat: Add CRUD with server actions
    Finish of chapter 11

--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -9,7 +9,7 @@ import { usePathname, useSearchParams } from "next/navigation";
 export default function Pagination({ totalPages }: { totalPages: number }) {
   // NOTE: Uncomment this code in Chapter 10
 
-  const pathName = usePathname();
+  const pathname = usePathname();
   const searchParams = useSearchParams();
   const currentPage = Number(searchParams.get("page")) || 1;
   const allPages = generatePagination(currentPage, totalPages);
